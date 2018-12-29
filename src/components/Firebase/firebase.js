701bd850b58cc4bf5085b021592b5049933301e9
CasePloeg <case.ploeg@mail.utoronto.ca>
2018-12-29 02:42:22 -0500

    moved config information to .env file

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: 'AIzaSyCf1avdUN7zcLl4Pb6xZi_vZG5UNO6vdWc',
-  authDomain: 'todolist-ba849.firebaseapp.com',
-  databaseURL: 'https://todolist-ba849.firebaseio.com',
-  projectId: 'todolist-ba849',
-  storageBucket: 'todolist-ba849.appspot.com',
-  messagingSenderId: '556406031549',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
